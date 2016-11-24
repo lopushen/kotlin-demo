a9c79e7d9557db104e0915aebb87b1d0ce3ff58b
Ievgenii Lopushen <ievgenii@backbase.com>
2016-11-24 12:35:34 +0200

    added missed operator overloading example

--- src/main/java/com/levi9/demo/presentation/ExtensionFunctionExample.kt
+++ src/main/java/com/levi9/demo/presentation/ExtensionFunctionExample.kt
@@ -1,6 +1,19 @@
 package com.levi9.demo.presentation
 
 fun main(args: Array<String>) {
+    val list = mutableListOf(1,2,3)
+
+    println(list)
+
+    list.swap(0,1)
+    println(list)
+
+    // WTF here if use String template the property does not work
+    // println("the last index is $list.lastIndex")
+    // but this works - calling method
+//    println("the last element is ${list.last()}")
+    println("the last index is " +list.lastIndex)
+
 
 }
 

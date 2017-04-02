f747040fb56ddc3d77958ee280e37f43434a00fc
Ievgenii Lopushen <lopushen@gmail.com>
2017-04-03 02:38:28 +0300

    nullable examples

--- src/main/java/com/levi9/demo/presentation/NullabilityExample.kt
+++ src/main/java/com/levi9/demo/presentation/NullabilityExample.kt
@@ -2,21 +2,22 @@ package com.levi9.demo.presentation
 
 // show the compile error here
 fun main(args: Array<String>) {
-    var str: String?
+    var str: String
     val nullableStr: String?
     val nullableStr2 = null
 
-    str = null // - not allowed
+//    str = nullableStr2!!
+//    str = null // - not allowed
 
     nullableStr = null
 
     str = try {nullableStr!!} catch (e:Exception){""}
-//    str = nullableStr!!
-//    str = nullableStr2!!
+
     str!!.length
-
-
-//    val l = if (b != null) b.length else -1
+//    val len: Int?
+//    val b = listOf(1,2,3,4)
+//    val l = if (b != null) b.size else -1
+//    l = len
 
 }
 

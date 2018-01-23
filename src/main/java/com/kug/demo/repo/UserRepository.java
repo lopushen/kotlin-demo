f24eab41a552e1b4f63d1f16b73173266c78e4ba
Ievgenii Lopushen <ievgenii@comergence.biz>
2018-01-23 21:18:13 +0200

    cleanup - made it work

--- src/main/java/com/kug/demo/repo/UserRepository.java
+++ src/main/java/com/kug/demo/repo/UserRepository.java
@@ -5,7 +5,4 @@ import com.kug.demo.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    User findByName(String name);
-    User findByNameAndId(String name, Long id);
-
 }
--- src/main/java/com/kug/demo/rest/UserController.java
+++ src/main/java/com/kug/demo/rest/UserController.java
@@ -5,7 +5,10 @@ import com.kug.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("user")
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController("/user")
 public class UserController {
 
     private UserService userService;
@@ -13,11 +16,6 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
-    }
-
-    @RequestMapping(path = "/{id}",method = RequestMethod.GET)
-    public User getUser(@PathVariable(value = "id") Long id) {
-                return userService.getUser(id);
     }
 
     @RequestMapping(method = RequestMethod.GET)
